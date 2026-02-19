1aaffd19f1bffdbdca2c06d317a7a5e8b2f38abe
James Shi <mrjamesshi@gmail.com>
2026-02-19 15:08:05 +0000

    File changes

--- src/pages/Consultant.jsx
+++ src/pages/Consultant.jsx
@@ -511,14 +511,6 @@ export default function Consultant() {
 
           {/* Messages */}
           <div className="flex-1 overflow-y-auto p-4 space-y-4">
-            {messages.length === 0 && (
-              <div className="flex items-center justify-center h-full text-center text-slate-500 px-8">
-                <div>
-                  <Sparkles className="h-12 w-12 text-slate-300 mx-auto mb-3" />
-                  <p className="text-sm">Start a conversation to find the perfect school for your child</p>
-                </div>
-              </div>
-            )}
             {messages.map((msg, index) => (
               <MessageBubble
                 key={index}
