6c057b2157b8e7649f47d099897758e995887e47
James Shi <mrjamesshi@gmail.com>
2026-02-20 19:17:35 +0000

    File changes

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -36,7 +36,7 @@ export default function Footer() {
           <div>
             <h3 className="text-white font-semibold text-sm mb-4">LEGAL</h3>
             <Link to={createPageUrl('Privacy')} className="block text-sm hover:text-white transition-colors mb-3">Privacy Policy</Link>
-            <a href="#" className="block text-sm hover:text-white transition-colors">Terms of Service</a>
+            <Link to={createPageUrl('Terms')} className="block text-sm hover:text-white transition-colors">Terms of Service</Link>
           </div>
         </div>
         
