74e333b1c3ee3d1d9a8c8905fb6b57f6e24698b7
James Shi <mrjamesshi@gmail.com>
2026-02-21 17:37:58 +0000

    File changes

--- src/pages/SchoolDirectory.jsx
+++ src/pages/SchoolDirectory.jsx
@@ -25,11 +25,26 @@ export default function SchoolDirectory() {
   const [sessionId] = useState(Math.random().toString(36).substring(2, 11));
   const SCHOOLS_PER_PAGE = 20;
   
-  // Get provinces based on selected country
-  const provincesByCountry = {
-    Canada: ['Alberta', 'BC', 'Manitoba', 'New Brunswick', 'Newfoundland', 'Nova Scotia', 'Ontario', 'PEI', 'Quebec', 'Saskatchewan'],
-    'United States': ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'],
-    'United Kingdom': ['England', 'Scotland', 'Wales', 'Northern Ireland']
+  // Dynamically extract provinces/states from schools filtered by country
+  const getAvailableProvinces = () => {
+    if (filterCountry === 'all') return [];
+    
+    const countryMap = {
+      'Canada': 'Canada',
+      'US': 'United States',
+      'UK': 'United Kingdom'
+    };
+    
+    const selectedCountryValue = countryMap[filterCountry];
+    const provincesSet = new Set();
+    
+    allSchools.forEach(school => {
+      if (school.country === selectedCountryValue && school.provinceState) {
+        provincesSet.add(school.provinceState);
+      }
+    });
+    
+    return Array.from(provincesSet).sort();
   };
 
   useEffect(() => {
